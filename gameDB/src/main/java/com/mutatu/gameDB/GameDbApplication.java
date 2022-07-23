15e8a33932f5caf7ce24091d91597ee11f68e88a
NetYai <NetYai@nowhere.none>
2022-07-22 21:41:59 -0300

    add the needed XSRF token into response cookies

--- gameDB/src/main/java/com/mutatu/gameDB/GameDbApplication.java
+++ gameDB/src/main/java/com/mutatu/gameDB/GameDbApplication.java
@@ -2,7 +2,11 @@ package com.mutatu.gameDB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -14,4 +18,13 @@ public class GameDbApplication {
 		SpringApplication.run(GameDbApplication.class, args);
 	}
 
+	//attach web request filter to include the anti-CRSF token on cookies
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(csrf -> csrf
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			);
+		return http.build();
+	}
 }
